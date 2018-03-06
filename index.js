8a07430bdacfbc5b7348c47b87aab6ca555cf16c
Brie Morales <bombibills@gmail.com>
2018-03-06 23:06:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,8 +91,7 @@ function createRock(x) {
      * the GAME, we want to move it again.
      */
      if (positionToInteger(rock.style.top) < 380){
-       console.log(rock.style.top);
-      rock.style.top = rock.style.top+1
+      rock.style.top = 10
       console.log(rock.style.top)
       console.log("moving rock...");
       window.requestAnimationFrame(moveRock);
