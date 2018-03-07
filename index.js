78681cf3125e246f196a2ccdc70320402e7e0319
Brie Morales <bombibills@gmail.com>
2018-03-07 00:33:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ function moveDodgerLeft() {
    stepLeft();
    function stepLeft(){
      if (leftNumbers > leftGetTo){
-       DODGER.style.left = `${positionToInteger(DODGER.style.left) - 4}px`
+       DODGER.style.left = `${leftNumbers - 4}px`
        window.requestAnimationFrame(stepLeft)
      }
    }
